c58dbc87fad32fcdaa5994a1ba4a7150fe3e173c
JaYRaNa213 <tresurejay6@gmail.com>
2025-03-22 13:28:02 +0530

    Fix refresh token validation to check for undefined value

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -170,7 +170,7 @@ const logoutUser = asyncHandler(async (req, res) => {
 const refreshAccessToken = asyncHandler(async (req, res) => {
   const incomingRefreshToken = req.cookies.refreshToken || req.body.refreshToken;
 
-  if(incomingRefreshToken === undefined){
+  if(!incomingRefreshToken){
     throw new ApiError(400, "unauthorized request");
   }
 
