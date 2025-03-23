9d8a01161a0742fc5d3726327eb01b4d4b9e5fe2
JaYRaNa213 <tresurejay6@gmail.com>
2025-03-23 22:38:52 +0530

    Add models for comments, likes, playlists, and tweets with necessary fields and relationships

---
+++ src/models/like.model.js
@@ -0,0 +1,28 @@
+import express from "express";
+import mongoose from "mongoose";
+
+const likeSchema = new mongoose.Schema
+(
+  {
+    tweet:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"Tweet"
+    },
+    
+    tweet:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"Tweet"
+    },
+
+    
+    likedBy:{
+      type:mongoose.Schema.Types.ObjectId,
+      ref:"User"
+    }
+
+},
+{timestamps:true}
+
+)
+
+export const Like = mongoose.model("Like",likeSchema)
